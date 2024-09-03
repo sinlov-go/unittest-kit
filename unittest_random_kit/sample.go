c94bb5c66caedbfe1cef86a2ac427fad1ec4959b
sinlov <sinlovgmppt@gmail.com>
2024-09-03 13:48:41 +0800

    fix: unittest_random_kit Random seed at same time
    use seed by time.Now().UnixNano()
    
    fix #7

--- unittest_random_kit/sample.go
+++ unittest_random_kit/sample.go
@@ -10,11 +10,11 @@ const randomStrLetterCnt = 62
 var randomStrLetters = []byte("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
 
 // RandomStr
-//
-//	new random string by cnt
+// new random string by cnt, seed by time.Now().UnixNano()
+// and is 0-9 A-Z a-z
 func RandomStr(cnt uint) string {
 	result := make([]byte, cnt)
-	rs := rand.New(rand.NewSource(time.Now().Unix()))
+	rs := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := range result {
 		index := rs.Intn(randomStrLetterCnt)
 		result[i] = randomStrLetters[index]
@@ -23,9 +23,8 @@ func RandomStr(cnt uint) string {
 }
 
 // RandomInt
-//
-//	new random int by max
+// new random int by max, seed by time.Now().UnixNano()
 func RandomInt(max int) int {
-	rs := rand.New(rand.NewSource(time.Now().Unix()))
+	rs := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return rs.Intn(max)
 }
